e744687d6f31114110a77834498a872f66f30cb4
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-09-10 22:28:53 +0600

    property delete working still

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run() {
                const result = await propertiesCollection.findOne(query)
                res.send(result)
           }) 
-          app.get('/propertyDelete/:id', async(req,res)=>{
+          app.delete('/propertyDelete/:id', async(req,res)=>{
                const id = req.params.id
                const query = { _id: new ObjectId(id)};
                
